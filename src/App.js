d0ed3590290c6f1922f23da9abc418f5ce91911b
Nirvan <93690524+Nirvs1@users.noreply.github.com>
2022-09-20 14:38:41 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { ethers } from "ethers";
+import { ethers } from "Ethers";
 import erc20abi from "./ERC20abi.json";
 import ErrorMessage from "./ErrorMessage";
 import TxList from "./TxList";
